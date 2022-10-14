2725e7075af231f7374ba39a662a9a5ac2715a6c
GeorgeURfinancial <115512217+GeorgeURfinancial@users.noreply.github.com>
2022-10-13 19:45:37 -0500

    Add files via upload

---
+++ Pythonjaa.py
@@ -0,0 +1,128 @@
+stringNombre = "jorge"
+stringApellido="espinosa"
+
+print(stringNombre,stringApellido)
+
+
+print("INGRESA NOMBRE")
+nombre = input("NOMBRE ")
+
+print("cual es tu edad")
+edad= int(input("edad"))
+
+
+"""
+if edad>18:
+    print("eres mayor")
+    if edad <18:
+        print("eres menor")
+        
+    
+    print("ingresa tu salario")
+    salario=(input("dime tu salario:) ")
+             
+        if salario>10,000,00:
+"""
+###############################################################################
+
+print("ingresa tu salario")
+salario= int(input("dime tu salario:) ")
+
+             
+
+             
+
+             
+
+             
+####################################################
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 13 18:01:20 2022
+
+@author: prestamour
+"""
+
+print("ingresa tu salario")
+salario= int(input("dime tu salario:) 
+if salario>4000000 and salario <1000000:
+    impuesto = 0.15
+elif salario>10000000 and salario<14000000:
+    impuesto
+    
+    ###################################################################
+    # -*- coding: utf-8 -*-
+    """
+    Created on Thu Oct 13 18:25:03 2022
+
+    @author: prestamour
+    """
+
+    miList = []
+
+    count =0
+    while count <=100:
+        miList.append(count)
+        count=count+1
+        
+        
+    ##############################################################################
+
+    print("Dime un nombre: ")
+    nombre=input("nombre: ")
+
+    milistaa = []
+    count=0
+    while count <=100:
+        milistaa.append(nombre)
+        count=count+1
+        
+        ###############################################################################
+        # -*- coding: utf-8 -*-
+        """
+        Created on Thu Oct 13 17:54:54 2022
+
+        @author: prestamour
+        """
+
+
+        milista=[]
+        count=0
+        while count <3:
+            #milista.append(count)
+            print("ingresa tu salario")
+            salario= int(input("dime tu salario: "))
+            if salario>4000000 and salario <10000000:
+                impuesto = 0.15
+                print(0.15*salario)
+                
+            elif salario>10000000 and salario<15000000:
+                impuesto=0.20
+                taxes=salario*impuesto
+                print(taxes)
+                
+            else:
+                impuesto=0.30
+                taxes=salario*impuesto
+                print(taxes)
+            count=count+1
+
+        ################################################
+
+        #print("¿Cúal es tu nombre?")
+        #print("pepito tiene que pagar $ ",(0.15*salario))
+        
+        ####################################################################
+        
+        # -*- coding: utf-8 -*-
+        """
+        Created on Thu Oct 13 19:04:14 2022
+
+        @author: prestamour
+        """
+        count=0
+        while count <3:
+            numeroUno=int(input("ingresa numero one: "))
+            numeroDos=int(input("ingresa numero Two: "))
+            print("El resutado de la suma es", numeroUno + numeroDos)
+            count=count+1
